52406f2ab7b74932eb0fbc4daf36b9ea6cae0297
haiyao <haiyao@student.unimelb.edu.au>
2022-10-08 20:05:56 +1100

    set up basic GUI layout

--- src/main/java/com/example/distributedsharedwhiteboard/HelloController.java
+++ src/main/java/com/example/distributedsharedwhiteboard/HelloController.java
@@ -1,14 +1,66 @@
 package com.example.distributedsharedwhiteboard;
 
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField message;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView MsgHistory;
+
+    @FXML
+    private Pane canvas;
+
+    @FXML
+    private Line line;
+
+    @FXML
+    protected void handleQuitButtonAction(ActionEvent event) {
+        Platform.exit();
+    }
+
+    @FXML
+    protected void handleSendMessageAction(ActionEvent event) {
+
+        // get msg
+        String msg = message.getText();
+
+        // check if text is empty
+        if (msg != null && msg.length() != 0) {
+
+            // add msg to message history
+            ObservableList<String> msgs = MsgHistory.getItems();
+            msgs.addAll("user54686: " + msg);
+            MsgHistory.setItems(msgs);
+            System.out.println("Message: " + msg);
+
+            // clear input
+            message.clear();
+        }
+    }
+
+    @FXML
+    protected void handleDrawAction(MouseEvent event) {
+
+        // check mouse event
+        if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+
+            line.setStartX(event.getX());
+            line.setStartY(event.getY());
+        }else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED || event.getEventType() == MouseEvent.MOUSE_RELEASED) {
+
+            line.setEndX(event.getX());
+            line.setEndY(event.getY());
+        }
     }
 }
