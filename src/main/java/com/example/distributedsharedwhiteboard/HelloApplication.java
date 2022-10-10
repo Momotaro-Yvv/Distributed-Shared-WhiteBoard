1df927cef5a380a4b4067731cc0acd25c786774c
yvonne tao <yuwtao@student.unimelb.edu.au>
2022-10-11 02:44:26 +1100

    Modify fxml files
    Add UI for server, create window for mgr approve/reject new user join

--- src/main/java/com/example/distributedsharedwhiteboard/HelloApplication.java
+++ src/main/java/com/example/distributedsharedwhiteboard/HelloApplication.java
@@ -1,6 +1,6 @@
 package com.example.distributedsharedwhiteboard;
 
-import javafx.application.Application;
+import  javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -21,6 +21,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
